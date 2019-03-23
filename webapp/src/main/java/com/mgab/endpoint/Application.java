adf1a40efb7c368f377b342ffeaa569f304510c5
MVgusar <MVgusar@users.noreply.github.com>
2019-03-23 17:52:43 +0300

    add spring context

--- webapp/src/main/java/com/mgab/endpoint/Application.java
+++ webapp/src/main/java/com/mgab/endpoint/Application.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = "applicationContext.xml")
 public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
